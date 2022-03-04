2a468ca78dc4695b90090275f77f1d1afa4ebca3
Ty Werbicki <86994333+tywerbicki@users.noreply.github.com>
2022-03-04 14:59:27 -0700

    Update reduce.cpp

--- reduce.cpp
+++ reduce.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
+#include <array>
 #include <immintrin.h>
-#include <thread>
-#include <mutex>
 #include <chrono>
 
-template <typename T>
-T sum(const T * __restrict__ X, const size_t n) {
+template <typename T, size_t n>
+T sum(const std::array<T,n>& X) {
 
     T sum = 0;
     for (size_t i = 0; i < n; i++) sum += X[i];
@@ -26,15 +25,17 @@ float _mm256_sum_reduction_ps(const __m256 vec) {
     return *((float*)&low);
 }
 
-float sum_simd(const float * __restrict__ X, const size_t n) {
+template<size_t n>
+float sum_simd(const std::array<float,n>& X) {
 
+    const float *X_r = X.data();
     const size_t stride = sizeof(__m256) / sizeof(float);
     const size_t border = n - (n % stride);
 
     __m256 _sum = _mm256_set1_ps(0.0F);
-    for (size_t i = 0; i < border; i += stride) _sum += _mm256_loadu_ps(X + i);
+    for (size_t i = 0; i < border; i += stride) _sum += _mm256_loadu_ps(X_r + i);
     float sum = _mm256_sum_reduction_ps(_sum);
-    for (size_t i = border; i < n; i++) sum += X[i];
+    for (size_t i = border; i < n; i++) sum += X_r[i];
     
     return sum;
 }
@@ -42,24 +43,20 @@ float sum_simd(const float * __restrict__ X, const size_t n) {
 
 int main() {
 
-    const size_t n = (2 << 9) + 7;
-    float* vec = new float[n];
+    const size_t n = (2 << 10) + 7;
+    std::array<float,n> vec;
     
     for (size_t i = 0; i < n; i++) vec[i] = 1.0F;
 
-    float result, result_simd;
-
     auto start = std::chrono::steady_clock::now();
-    result = sum<float>(vec, n);
+    float result = sum<float,n>(vec);
     auto end = std::chrono::steady_clock::now();
     std::chrono::duration<double> duration = end - start;
 
     start = std::chrono::steady_clock::now();
-    result_simd = sum_simd(vec, n);
+    float result_simd = sum_simd<n>(vec);
     end = std::chrono::steady_clock::now();
     std::chrono::duration<double> duration_simd = end - start;
-
-    delete[] vec;
 
     std::cout << "Sum: " << result << "  Duration: " << duration.count() << "\n";
     std::cout << "Sum_simd: " << result_simd << "  Duration: " << duration_simd.count() << std::endl;
